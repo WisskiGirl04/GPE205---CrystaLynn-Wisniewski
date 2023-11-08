5c7a68dec92a497e5cbbfd9f13cb8c84800d6602
CrystaLynn Wisniewski <CWISNIEWSKI85911@uat.edu>
2023-11-08 09:21:31 -0500

    imported tile templates, implemented room generator

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public GameObject[] gridPrefabs;
+    public int rows;
+    public int collums;
+    public float roomWidth = 50.0f;
+    public float roomHeight = 50.0f;
+    private Room[,] grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Test map generation with keypress
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            GenerateMap();
+        }
+    }
+
+    // Returns a random room
+    public GameObject RandomRoomPrefab()
+    {
+        return gridPrefabs[Random.Range(0, gridPrefabs.Length)];
+    }
+
+    public void GenerateMap()
+    {
+        // Clear out the grid - "column" is our X, "row" is our Y
+        grid = new Room[collums, rows];
+
+        // For each grid row...
+        for (int currentRow = 0; currentRow < rows; currentRow++)
+        {
+            // for each column in that row
+            for (int currentCol = 0; currentCol < collums; currentCol++)
+            {
+                // Figure out the location and save that location in new local variables 
+                float xPosition = roomWidth * currentCol;
+                float zPosition = roomHeight * currentRow;
+                Vector3 newPosition = new Vector3(xPosition, 0.0f, zPosition);
+
+                // Create/spawn a new grid/tile at the appropriate location
+                GameObject tempRoomObj = Instantiate(RandomRoomPrefab(), newPosition, Quaternion.identity) as GameObject;
+
+                // Set its parent
+                tempRoomObj.transform.parent = this.transform;
+
+                // Give it a meaningful name
+                tempRoomObj.name = "Room_" + currentCol + "," + currentRow;
+
+                // Get the room object
+                Room tempRoom = tempRoomObj.GetComponent<Room>();
+
+                // Open the doors
+                //Rows
+                // If we are on the bottom row, open the north door
+                if (currentRow == 0)
+                {
+                    tempRoom.doorNorth.SetActive(false);
+                }
+                else if (currentRow == rows - 1)
+                {
+                    // Otherwise, if we are on the top row, open the south door
+                    tempRoom.doorSouth.SetActive(false);
+                }
+                else
+                {
+                    // Otherwise, we are in the middle, so open both doors
+                    tempRoom.doorNorth.SetActive(false);
+                    tempRoom.doorSouth.SetActive(false);
+                }
+                //Collums
+                if(currentCol == 0)
+                {
+                    tempRoom.doorEast.SetActive(false);
+                }
+                else if (currentCol == collums - 1)
+                {
+                    tempRoom.doorWest.SetActive(false);
+                }
+                else
+                {
+                    tempRoom.doorEast.SetActive(false);
+                    tempRoom.doorWest.SetActive(false);
+                }
+
+
+                // Save it to the grid array
+                grid[currentCol, currentRow] = tempRoom;
+            }
+        }
+    }
+
+}
