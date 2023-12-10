24960104dec74d4cd5c995e093168ccfcb8fce10
CrystaLynn Wisniewski <CWISNIEWSKI85911@uat.edu>
2023-12-10 12:29:23 -0500

    pickup audio now works
    - added a delay to destroy so that audio has a chance to play before the object is destroyed

--- Assets/Scripts/PowerUps/HealthPickup.cs
+++ Assets/Scripts/PowerUps/HealthPickup.cs
@@ -7,66 +7,53 @@ public class HealthPickup : MonoBehaviour
     public HealthPowerup powerup;
     public AudioClip pickupClip;
     public AudioSource audioSource;
+    private float destroyDelay;
+    private float destroyingTime;
+    private float ogTriggerCount;
+    private float nextTriggerCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = gameObject.GetComponent<AudioSource>();
+        destroyDelay = audioSource.clip.length / 3f;
+        ogTriggerCount = 0;
+        nextTriggerCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+        if (nextTriggerCount != 0 && ogTriggerCount < nextTriggerCount)
+        {
+            if (Time.time >= destroyingTime)
+            {
+                Debug.Log("Time is " + Time.time + " and Destroying time!");
+                ogTriggerCount++;
+                Destroy(gameObject);
+            }
+        }
     }
 
     public void OnTriggerEnter(Collider other)
     {
         // Variable to store other object's PowerupManager (PowerUpController) - if it has one
         PowerUpManager powerupManager = other.GetComponent<PowerUpManager>();
+        Debug.Log("on trigger hit the time is " + Time.time);
+        nextTriggerCount++;
 
-
-        audioSource = gameObject.GetComponent<AudioSource>();
-        audioSource.Play();
-        Debug.Log(audioSource.isPlaying);
-
-        if (audioSource.isPlaying == false)
-        {
-            Debug.Log(audioSource.isPlaying);
-        }
+        destroyingTime = Time.time + destroyDelay;
         // If our variable is not equal to null (the other object has a PowerUpManager and it is stored)cv 
         if (powerupManager != null)
         {
-            audioSource = gameObject.GetComponent<AudioSource>();
-            Debug.Log(other);
-            Debug.Log(other.name);
-            Vector3 playLocation = gameObject.transform.position;
-            AudioClip clip = gameObject.GetComponent<AudioSource>().clip;
-            Debug.Log("play called");
+            audioSource.Play();
             if (other.GetComponent<Health>().currentHealth < other.GetComponent<Health>().maxHealth)
             {
                 // Add the powerup
                 powerupManager.Add(powerup);
             }
-            audioSource.Play();
-            Debug.Log(audioSource.isPlaying);
-            if (audioSource.isPlaying == false)
-            {
-                Debug.Log(audioSource.isPlaying);
-            }
-            //StartCoroutine(WaitTillClipEnd());
-            float clipLength = audioSource.clip.length;
-            
-                // Destroy this pickup
-                Destroy(this.gameObject);
-            // Destroy this pickup
-            //Destroy(this.gameObject);
         }
-    }
-
-    IEnumerator WaitTillClipEnd()
-    {
-        Debug.Log("WaitTillClipEnd called");
-        yield return new WaitWhile(() => audioSource.isPlaying);
     }
 }
